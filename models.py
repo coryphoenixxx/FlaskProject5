5b7d869b99c439eab0f93bb527befdd222e82203
coryphoenixxx <coryphoenixxx@gmail.com>
2020-11-14 09:22:31 +0700

    some changes

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.security import check_password_hash
 
 
 db = SQLAlchemy()
